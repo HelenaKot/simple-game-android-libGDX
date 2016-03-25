c326e2caa8296b05f650429d24b93d5e2193cbc3
rilane <s10474@pjwstk.edu.pl>
2016-03-25 07:24:29 +0100

    added Viewport to stage

--- core/src/com/mygdx/game/Block.java
+++ core/src/com/mygdx/game/Block.java
@@ -10,9 +10,10 @@ import com.badlogic.gdx.scenes.scene2d.Actor;
 import java.util.Iterator;
 
 public class Block extends Actor {
-    private static float scale = 10, actorX = 0, actorY = 0;
+    private static float actorX = 0, actorY = 0;
     private Sprite sprite; //TODO dispose this
 
+    //TODO actor = instance not block
     Block(TextureAtlas.AtlasRegion region, Color tint) {
         sprite = new Sprite(region);
         sprite.setColor(tint);
@@ -29,14 +30,10 @@ public class Block extends Actor {
 
     @Override
     public void draw(Batch batch, float alpha){
-        batch.draw(sprite,actorX,actorY);
-        batch.draw(sprite.getTexture(),this.getX(),getY(),this.getOriginX(),this.getOriginY(),this.getWidth(),
-                this.getHeight(),this.getScaleX(), this.getScaleY(),this.getRotation(),0,0,
-                (int) sprite.getWidth(), (int) sprite.getHeight(),false,false);
+        batch.draw(sprite,actorX,actorY,this.getOriginX(),this.getOriginY(), this.getWidth(),
+                this.getHeight(),this.getScaleX(), this.getScaleY(),this.getRotation());
     }
 
-    //myActor.addAction(parallel(STUFF)/sequence(STUF))
-    // STUFF fe : scaleTo, rotateTo, moveTo
     @Override
     public void act(float delta){
         for(Iterator<Action> iter = this.getActions().iterator(); iter.hasNext();){
--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -6,6 +6,8 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.Touchable;
+import com.badlogic.gdx.utils.viewport.ExtendViewport;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
 
 public class MyGdxGame extends ApplicationAdapter {
     TextureAtlas textureAtlas;
@@ -16,18 +18,22 @@ public class MyGdxGame extends ApplicationAdapter {
         Gdx.gl.glClearColor(0.3f, 0.72f, 0.7f, 1);
         textureAtlas = new TextureAtlas(Gdx.files.internal("pack.atlas"));
         MapAtlas.createMapAlas(textureAtlas);
-        stage = new Stage();
+        stage = new Stage(new ExtendViewport(128,64));
         Gdx.input.setInputProcessor(stage);
-        Block sampleActor = MapAtlas.instance.get(4);
+        Block sampleActor = MapAtlas.instance.get(10);
         sampleActor.setTouchable(Touchable.enabled);
         stage.addActor(sampleActor);
     }
 
     @Override
+    public void resize (int width, int height) {
+        // See below for what true means.
+        stage.getViewport().update(width, height, true);
+    }
+
+    @Override
     public void render() {
-
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
         stage.act(Gdx.graphics.getDeltaTime());
         stage.draw();
     }
